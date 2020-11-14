4693ccd4a4441c686aa28f5d921cdc681650dd66
Juan Monterrosa Sanchez <38740763+jcmonterrosas@users.noreply.github.com>
2020-11-13 22:37:16 -0500

    ✏️ Hot fix

---
+++ users/src/test/java/com/user_cp/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user_cp.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
